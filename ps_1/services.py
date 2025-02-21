625bc8eb74f508e4322c581f2ccce6b1048c3a26
Paramaguru.H <paramaguruvh@gmail.com>
2025-02-22 01:39:40 +0530

    added feedback

--- ps_1/services.py
+++ ps_1/services.py
@@ -9,3 +9,9 @@ def model():
     final_prompt = ChatPromptTemplate.from_messages([("system", prompt()), ("human", "{input}")])
     few_shot_structured_llm = final_prompt | structured_llm
     return few_shot_structured_llm
+
+def feedback(question):
+    llm=ChatGroq(model="Gemma2-9b-It")
+    prompt=f"Give a overall response based on the sentiment analysis based on tone and language of the feedback, such that if negative give a remedy how can we improve it , if good then give a thank you message Feedback{question}"
+    response=llm.invoke(prompt)
+    return response
